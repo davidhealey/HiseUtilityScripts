63dc101c53430e19b6a5acac74a098977a748ba9
d-healey <dave41264@hotmail.com>
2018-01-03 00:03:27 +0000

    Removed keyboard colour resetting

--- libraries/instrumentDataHandler.js
+++ libraries/instrumentDataHandler.js
@@ -1,3 +1,20 @@
+/*
+    Copyright 2018 David Healey
+
+    This library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 include("instrumentData.js");
 
 namespace idh
@@ -10,13 +27,7 @@ namespace idh
 		local entry = instData.database[name]; //Get instrument entry from the database
 		
 		Console.assertIsObjectOrArray(entry); //Error if entry not found
-		
-		//Reset all key colours
-		for (i = 0; i < 127; i++)
-		{
-			Engine.setKeyColour(i, Colours.withAlpha(Colours.white, 0.0));
-		}
-		
+			
 		articulationIndexes = indexArticulations(name);
 		bypassUnusedSamplers(entry);
 		if (sampleMaps == true) loadSampleMaps(name, entry);
