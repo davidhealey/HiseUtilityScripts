7e24d4ced23e94e165f3c8267dda4ecb3464dc05
d-healey <dave41264@hotmail.com>
2018-03-18 16:57:46 +0000

    Updated comments

--- libraries/asyncUpdater.js
+++ libraries/asyncUpdater.js
@@ -5,10 +5,18 @@
 * License: Public Domain
 */
 
-/** The AsyncUpdater can be used to trigger a deferred UI update
-*	from a MIDI callback. Just give it a (inline) function using setUpdateFunction()
-*	and call triggerUpdate() to asynchronously launch the update function.
+/*
+This library is only of benefit in a script that does not have deferred callbacks enabled.
+
+The purpose of this library is to trigger a function asynchronously from a MIDI callback.
+You'll usually want to use this to update the GUI from with onNoteOn, onNoteOff or onController.;
+Updating the GUI from one of these callbacks can cause audio dropouts because these callbacks run on the
+real-time thread, this library solves that by providing a method to defer a function using a timer object.;
+
+This library only supports the use of one deferred function at a time, to defer multiple functions you will need to
+either combine those functions into one or modify this library to allow for multiple functions.
 */
+
 namespace asyncUpdater
 {
 	const var updater = Engine.createTimerObject();
@@ -23,7 +31,9 @@ namespace asyncUpdater
         }
         this.stopTimer();
     };
-    
+
+    //This is the only function you need to call. Pass in the name of the function you want to defer (f)
+    // and if needed a parameter (p). If no parameter is required then set p to null.
 	inline function deferFunction(f, p)
 	{
 		func = f;
