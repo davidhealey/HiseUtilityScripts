89e0ffad974e4c8209a664557d2d34646b28d4de
d-healey <dave41264@hotmail.com>
2018-05-17 17:40:52 +0100

    Minor change

--- libraries/uiFactory.js
+++ libraries/uiFactory.js
@@ -21,7 +21,7 @@ namespace ui
 		{
 			if (event.clicked)
 			{
-			    this.setValue(parseInt((this.getValue() + 1)) % parseInt(this.get("max") + 1));
+				this.setValue(parseInt((this.getValue() + 1)) % parseInt(this.get("max") + 1));
 				this.changed(); // tells the script to execute the onControl callback with the new value
 				this.repaint();
 			}
@@ -48,14 +48,14 @@ namespace ui
             {
                 this.setValue(0);
             }
-    
+
             this.changed();
             this.repaint();
 		});
 
 		return control;
-	};	
-	
+	};
+
 	inline function comboBoxPanel(id, paintRoutine, fontSize, items) //Custom combo box
 	{
 		local control = Content.getComponent(id);
@@ -96,13 +96,13 @@ namespace ui
         local presetList = Engine.getUserPresetList(); //Get list of all user presets
         local presetNames = [];
         local l;
-    
+
         for (p in presetList)
         {
-            l = p.split("/");        
+            l = p.split("/");
             if (l.length == 3) presetNames.push(l[2]);
         }
-    
+
         return presetNames;
     };
 
@@ -163,33 +163,33 @@ namespace ui
 
 		return control;
 	};
-	
+
     //License - Public Domain
     inline function modWheel(id, sensitivity)
     {
         local control = Content.getComponent(id);
 
-        control.set("allowCallbacks", "Clicks, Hover & Dragging");        
+        control.set("allowCallbacks", "Clicks, Hover & Dragging");
 		control.setPaintRoutine(paintRoutine);
 		control.data.sensitivity = sensitivity;
 		control.setValue(0);
 
 		control.setPaintRoutine(function(g){
-		    
+
             g.fillAll(this.get("bgColour"));
-    
+
             var lineSize = this.get("borderSize");
             var linePos = Math.range(this.get("height")-((this.get("height")/this.get("max")) * this.getValue()), lineSize/2, this.get("height")-lineSize/2);
 
             //Draw value
             g.setColour(this.get("itemColour"));
             g.fillRect([0, linePos, this.get("width"), this.get("height")]);
-        
+
             //Draw line
             g.setColour(this.get("itemColour2"));
             g.drawLine(0, this.get("width"), linePos, linePos, lineSize);
 		});
-		
+
         control.setMouseCallback(function(event)
         {
             if (event.clicked)
@@ -199,20 +199,20 @@ namespace ui
             else
             {
                 var newValue = this.getValue();
-                
+
                 if (event.drag)
                 {
                     var distance  = (event.dragX - event.dragY) / this.data.sensitivity; // Calculate the sensitivity value
                     newValue = Math.range(this.data.mouseDownValue + distance, this.get("min"), this.get("max"));
                 }
-                        
+
                 if (newValue != this.getValue()) //The value has changed
                 {
                     this.setValue(newValue);
                     this.repaint();
                     this.changed();
                     Synth.sendController(1, newValue);
-                }                
+                }
             }
         });
 
@@ -224,37 +224,37 @@ namespace ui
     {
         local control = Content.getComponent(id);
 
-        control.set("allowCallbacks", "Clicks, Hover & Dragging");        
+        control.set("allowCallbacks", "Clicks, Hover & Dragging");
 		control.setPaintRoutine(paintRoutine);
 		control.data.sensitivity = sensitivity;
 		control.setValue(8192);
 
 		control.setPaintRoutine(function(g){
             var lineSize = this.get("borderSize");
-    
+
             //Calcluate the position to draw the line based on the panel's value (pitch wheel value)
             //use Math.range to account for the height of the line (lineSize)
             var linePos = Math.range(this.get("height")-((this.get("height")/this.get("max")) * this.getValue()), lineSize/2, this.get("height")-lineSize/2);
-    
+
             g.fillAll(this.get("bgColour"));
-            
+
             //Draw value display
             g.setColour(this.get("itemColour"));
-    
+
             if (this.getValue() > 8192) //Bend up
-            {   
+            {
                 g.fillRect([0, linePos, this.get("width"), this.get("height")/2 - linePos]);
             }
             else if (this.getValue() < 8192) //Bend down
             {
                 g.fillRect([0, this.get("height")/2, this.get("width"), linePos-this.get("height")/2]);
             }
-        
+
             //Draw line
             g.setColour(this.get("itemColour2"));
             g.drawLine(0, this.get("width"), linePos, linePos, lineSize);
 		});
-		
+
         control.setMouseCallback(function(event)
         {
             if (event.clicked)
@@ -264,25 +264,25 @@ namespace ui
             else
             {
                 var newValue = this.getValue();
-                
+
                 if (event.drag)
                 {
                     var distance  = ((event.dragX - event.dragY) * 150) / this.data.sensitivity; // Calculate the sensitivity value
                     newValue = Math.range(this.data.mouseDownValue + distance, this.get("min"), this.get("max"));
                 }
-                
+
                 if (event.mouseUp)
                 {
                     newValue = 8192;
                 }
-                
+
                 if (newValue != this.getValue()) //The value has changed
                 {
                     this.setValue(newValue);
                     this.repaint();
                     this.changed();
                     Synth.sendController(129, newValue);
-                }   
+                }
             }
         });
 
@@ -295,16 +295,16 @@ namespace ui
 		{
 			a[i].set("visible", false);
 		}
-		idx < a.length ? a[idx].set("visible", true) : Console.print("showControlFromArray: index out of range");	    
+		idx < a.length ? a[idx].set("visible", true) : Console.print("showControlFromArray: index out of range");
 	};
-	
+
 	inline function setupControl(id, json)
     {
         local control = Content.getComponent(id);
         Content.setPropertiesFromJSON(id, json);
         return control;
     };
-    
+
     inline function setComboPanelItems(id, items)
     {
         local control = Content.getComponent(id);
