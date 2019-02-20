11fc9eb2c18b3de77fd908b0209cc22c91e2a1c4
davidhealey <dave41264@hotmail.com>
2019-02-20 14:04:24 +0000

    Minor change to make sure middle position is in knob range

--- modules/coarseFineTuner.js
+++ modules/coarseFineTuner.js
@@ -7,14 +7,14 @@ Content.setHeight(50);
 const var knbFine = Content.addKnob("knbFine", 0, 0);
 knbFine.set("text", "Fine Tune");
 knbFine.set("suffix", "ct");
-knbFine.set("middlePosition", 0);
 knbFine.setRange(-100, 100, 1);
+knbFine.set("middlePosition", 0);
 
 const var knbCoarse = Content.addKnob("knbCoarse", 150, 0);
 knbCoarse.set("text", "Coarse Tune");
 knbCoarse.set("suffix", "st");
-knbCoarse.set("middlePosition", 0);
 knbCoarse.setRange(-12, 12, 1);function onNoteOn()
+knbCoarse.set("middlePosition", 0);
 {
 	Message.setFineDetune(knbFine.getValue());
 	Message.setCoarseDetune(knbCoarse.getValue());
