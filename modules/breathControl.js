2d0db588809af823ee2a60f4b0358fd62b9cc024
davidhealey <dave41264@hotmail.com>
2019-07-01 01:43:08 +0100

    Added support to control velocity with breath speed

--- modules/breathControl.js
+++ modules/breathControl.js
@@ -18,11 +18,14 @@
 Content.setWidth(600);
 Content.setHeight(50);
 
-const var ids = Engine.createMidiList();
+const var velo = Engine.createMidiList(); //Note on velocities
+const var ids = Engine.createMidiList(); //Note Ids
 ids.fill(false);
 
-reg lastLevel = 0;
+reg lastLevel = 0; //Last breath level
+reg lastTime = 0;
 
+//GUI
 const var btnBypass = Content.addButton("btnBypass", 10, 10);
 btnBypass.set("text", "Bypass");
 
@@ -36,34 +39,63 @@ knbLevel.set("text", "Level");
 knbLevel.setRange(0, 127, 1);
 knbLevel.setControlCallback(onknbLevelControl);
 
+const var btnVelMode = Content.addButton("btnVelMode", 460, 10)
+btnVelMode.set("text", "Speed = Velocity");
+
 inline function onknbLevelControl(component, value)
 {
     if (!btnBypass.getValue())
     {
         local threshold = knbThreshold.getValue();
+        local velocity = 1;
+        
+        //If btnVelMode enabled calculate the velocity
+        if (btnVelMode.getValue())
+            velocity = getVelocity();
     
+        //Going up but haven't reached threshold, update lastTime
+        if (lastTime == 0 && value < threshold && value > lastLevel)
+            lastTime = Engine.getUptime();
+            
         for (i = 0; i < 127; i++)
         {
             if (Synth.isKeyDown(i))
             {
                 if (value >= threshold && lastLevel < threshold)
                 {
+                    //Turn off existing note (if any)
                     if (ids.getValue(i) != false)
                         Synth.noteOffByEventId(ids.getValue(i));
             
-                    ids.setValue(i, Synth.playNote(i, 64));  
+                    //Get velocity for note on if btnVelMode disabled
+                    if (!btnVelMode.getValue())
+                        velocity = velo.getValue(i);
+                        
+                    //Play new note
+                    ids.setValue(i, Synth.playNote(i, velocity));
                 }
                 else if (value < threshold && ids.getValue(i) != false)
                 {
+                    //Turn off note
                     Synth.noteOffByEventId(ids.getValue(i));
                     ids.setValue(i, false);
+                    lastTime = 0;
                 }
             }
         }
-    
-        lastLevel = value;   
+
+        //Update lastLevel value
+        lastLevel = value;
     }
-};function onNoteOn()
+};
+
+//Calculate the velocity based on the breath speed - up to 1 second window
+inline function getVelocity()
+{
+    local timeDiff = Math.min(1, Engine.getUptime()-lastTime) / 1; //Limit timeDiff to 0-1 seconds
+    local v = Math.pow(timeDiff, 0.3); //Skew value
+    return parseInt(127-(v*117));
+}function onNoteOn()
 {        
     if (!btnBypass.getValue())
     {
@@ -75,7 +107,9 @@ inline function onknbLevelControl(component, value)
                 Synth.noteOffByEventId(ids.getValue(Message.getNoteNumber()));
             
             ids.setValue(Message.getNoteNumber(), Message.makeArtificial());
-        }        
+        }
+        
+        velo.setValue(Message.getNoteNumber(), Message.getVelocity());
     }        
 }
  function onNoteOff()
@@ -84,6 +118,7 @@ inline function onknbLevelControl(component, value)
     {
         Synth.noteOffByEventId(ids.getValue(Message.getNoteNumber()));
         ids.setValue(Message.getNoteNumber(), false);
+        lastTime = 0;
     }
 }
  function onController()
