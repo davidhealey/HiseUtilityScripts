bf8b08aef6d7f3e9e80c2ab41ff332dc47333c36
davidhealey <dave41264@hotmail.com>
2020-07-16 17:30:30 +0100

    Wrapepd into a namespace with convenience functions

--- libraries/preloadBar.js
+++ libraries/preloadBar.js
@@ -1,26 +1,45 @@
 //License: Public Domain
-//Author: Christoph Hart
+//Author: Christoph Hart + David Healey
 
-const var preloadBar = Content.getComponent("preloadBar");
-preloadBar.setPaintRoutine(function(g)
+namespace PreloadBar
 {
-	g.fillAll(this.get("bgColour"));
-	g.setColour(this.get("itemColour"));
-	g.fillRect([0, 0, this.getWidth() * this.data.progress, this.getHeight()]);
-});
+    var _component;
 
-preloadBar.setTimerCallback(function()
-{
-	this.data.progress = Engine.getPreloadProgress();
-	this.repaint();	
-});
+    inline function init(panelId, props)
+    {
+        _component = Content.getComponent(panelId);
 
-preloadBar.setLoadingCallback(function(isPreloading)
-{
-    this.data.progress = 0.0;
-    this.set("visible", isPreloading);
-	if(isPreloading)
-        this.startTimer(30);
-    else
-        this.stopTimer();
-});
+        if (typeof props == "object")
+        {
+            for (p in props)
+            {
+                _component.set(p, props[p]);
+            }
+        }
+        
+        _component.setPaintRoutine(function(g)
+        {
+	        g.fillAll(this.get("bgColour"));
+	        g.setColour(this.get("itemColour"));
+	        g.fillRect([0, 0, this.getWidth() * this.data.progress, this.getHeight()]);
+        });
+        
+        _component.setTimerCallback(function()
+        {
+            this.data.progress = Engine.getPreloadProgress();
+            this.repaint();	
+        });
+        
+        _component.setLoadingCallback(function(isPreloading)
+        {
+            this.data.progress = 0.0;
+            this.set("visible", isPreloading);
+            isPreloading ? this.startTimer(30) : this.stopTimer();
+        });
+    }
+    
+    inline function setPosition(x, y, w, h)
+    {
+        _component.setPosition(x, y, w, h);
+    }
+}
