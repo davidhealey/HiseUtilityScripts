1c45a73bb68ba52c1936e3b02ec9c7d7d1354cf0
davidhealey <dave41264@hotmail.com>
2020-08-22 18:27:41 +0100

    Improved hanging note situation and made compatible with chords

--- modules/cc64Retrigger.js
+++ modules/cc64Retrigger.js
@@ -2,15 +2,17 @@
  * Title: cc64Retrigger
  * Author: David Healey
  * Date: 28/03/2017
- * Updated: 13/08/2020
+ * Updated: 22/08/2020
  * License: Public Domain
 */
 
 //INIT
 Content.setWidth(650);
 
-const var btnMute = Content.addButton("Mute", 10, 10);
+const var eventId = Engine.createMidiList();
+eventId.fill(-99);
 
+const var btnMute = Content.addButton("Mute", 10, 10);
 const var velocities = Engine.createMidiList();
 
 //FUNCTIONS
@@ -18,22 +20,46 @@ const var velocities = Engine.createMidiList();
 //CALLBACKS
 function onNoteOn()
 {
-	velocities.setValue(Message.getNoteNumber(), Message.getVelocity());
-}
+    local n = Message.getNoteNumber();
 
-function onNoteOff()
+    //Turn off retriggered note if there is one
+    if (eventId.getValue(n) != -99)
+    {
+        Synth.noteOffByEventId(eventId.getValue(n));
+        eventId.setValue(n, -99);
+    }
+
+	velocities.setValue(n, Message.getVelocity());
+}function onNoteOff()
 {
-	if (!btnMute.getValue() && (Synth.isSustainPedalDown() && velocities.getValue(Message.getNoteNumber()) > 0))
+	if (!btnMute.getValue())
 	{
-		Synth.playNote(Message.getNoteNumber(), velocities.getValue(Message.getNoteNumber()));
+        local n = Message.getNoteNumber();
+
+	    if (Synth.isSustainPedalDown() && velocities.getValue(n) > 0)
+	    {
+            if (eventId.getValue(n) != -99)
+	        {
+                Synth.noteOffByEventId(eventId.getValue(n));
+                eventId.setValue(n, -99);
+	        }
+
+            eventId.setValue(n, Synth.playNote(n, velocities.getValue(n)));
+	    }
 	}
-}
-
-function onController()
+}function onController()
 {
+    if (Message.getControllerNumber() == 64)
+    {
+        Message.ignoreEvent(true);
 
-}
- function onTimer()
+		if (!Synth.isSustainPedalDown())
+        {
+            Engine.allNotesOff();
+            eventId.fill(-99);
+        }
+    }
+}function onTimer()
 {
 
 }
@@ -41,4 +67,3 @@ function onController()
 {
 
 }
-
