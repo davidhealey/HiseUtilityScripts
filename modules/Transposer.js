80ba8812fe67b2e31f16f3ce2cad453d355f8941
dave <dave41264@hotmail.com>
2021-03-15 22:24:33 +0000

    Remade module with octave and semi-tone knobs

--- modules/Transposer.js
+++ modules/Transposer.js
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright 2019 David Healey
+Copyright 2021 David Healey
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute,
@@ -16,29 +16,40 @@ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
 DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-const var knbTransposition = Content.addKnob("Transposition", 0, 0);
-knbTransposition.setRange(-12, 12, 1);
-knbTransposition.set("text", "Transposition");
-knbTransposition.set("defaultValue", 0);
-knbTransposition.set("middlePosition", 0);
-function onNoteOn()
+
+const knbOctave = Content.addKnob("Octave", 0, 0);
+knbOctave.setRange(-2, 2, 1);
+knbOctave.set("middlePosition", 0);
+
+const knbSemi = Content.addKnob("SemiTone", 150, 0);
+knbSemi.set("text", "Semi Tone");
+knbSemi.setRange(-12, 12, 1);
+knbSemi.set("middlePosition", 0);function onNoteOn()
 {
-	if (knbTransposition.getValue() != 0)
-        Message.setTransposeAmount(knbTransposition.getValue());
-}function onNoteOff()
+	if (knbOctave.getValue() || knbSemi.getValue())
+    {
+        local value = 12 * knbOctave.getValue() + knbSemi.getValue();
+        Message.setTransposeAmount(value);
+    }
+}
+ function onNoteOff()
 {
-	if (knbTransposition.getValue() != 0)
-        Message.setTransposeAmount(knbTransposition.getValue());
+	if (knbOctave.getValue() || knbSemi.getValue())
+    {
+        local value = 12 * knbOctave.getValue() + knbSemi.getValue();
+        Message.setTransposeAmount(value);
+    }
 }
  function onController()
 {
-
+	
 }
  function onTimer()
 {
-
+	
 }
  function onControl(number, value)
 {
-
+	
 }
+ 
