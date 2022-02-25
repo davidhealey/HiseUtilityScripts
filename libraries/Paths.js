0ea00b5342616ccfc3f909f5a9641ac133dd2d1e
dave <dave41264@hotmail.com>
2022-02-25 01:41:12 +0000

    Updated copyright

--- libraries/Paths.js
+++ libraries/Paths.js
@@ -1,5 +1,5 @@
 /*
-    Copyright 2021 David Healey
+    Copyright 2021, 2022 David Healey
 
     This file is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
