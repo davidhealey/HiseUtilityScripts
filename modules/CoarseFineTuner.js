1f8c02098adbab497ba263e5066f1dd7c873757b
dave <dave41264@hotmail.com>
2022-06-08 14:36:07 +0100

    Use Engine.setGlobalPitchFactor instead of detuning individual notes

--- modules/CoarseFineTuner.js
+++ modules/CoarseFineTuner.js
@@ -1,39 +1,62 @@
 //License - Public Domain
-//David Healey 2019
+//David Healey 2019, 2022
 
 Content.setWidth(730);
 Content.setHeight(50);
 
+// knbFine
 const var knbFine = Content.addKnob("Fine", 0, 0);
 knbFine.set("text", "Fine");
 knbFine.set("suffix", "ct");
 knbFine.setRange(-100, 100, 1);
 knbFine.set("middlePosition", 0);
+knbFine.setControlCallback(onknbFineControl);
 
+inline function onknbFineControl(component, value)
+{
+	updateTuning();
+}
+
+// knbCoarse
 const var knbCoarse = Content.addKnob("Coarse", 150, 0);
 knbCoarse.set("text", "Coarse");
 knbCoarse.set("suffix", "st");
 knbCoarse.setRange(-12, 12, 1);
 knbCoarse.set("middlePosition", 0);
+knbCoarse.setControlCallback(onknbCoarseControl);
+
+inline function onknbCoarseControl(component, value)
+{
+	updateTuning();
+}
+
+// Functions
+inline function updateTuning()
+{
+	local fine = knbFine.getValue();
+	local coarse = knbCoarse.getValue();	
+	local v = coarse + fine / 100;
+
+	Engine.setGlobalPitchFactor(v);
+}
 function onNoteOn()
 {
-	Message.setFineDetune(knbFine.getValue());
-	Message.setCoarseDetune(knbCoarse.getValue());
+
 }
 function onNoteOff()
 {
-    Message.setFineDetune(knbFine.getValue());
-	Message.setCoarseDetune(knbCoarse.getValue());
+
 }
 function onController()
 {
-
+	
 }
-function onTimer()
+ function onTimer()
 {
-
+	
 }
-function onControl(number, value)
+ function onControl(number, value)
 {
-
+	
 }
+ 
